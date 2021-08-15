0fec6d02b6ab23b8945889d19801d7091cd0cca4
musangisilvia <musangisilvia@gmail.com>
2021-08-15 10:23:08 +0000

    include the client

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,13 @@
 # set up client ssh config to connect to server without typing password
 
-class { 'ssh::client':
-  options => {
-    'Host *' => {
-      'PasswordAuthentication'    => 'no',
-      'IdentityFile'              => '~/.ssh/holberton',
-      'SendEnv'                   => 'LANG LC_*'
-      'HashKnownHosts'            => 'yes'
-      'GSSAPIAuthentication'      => 'yes'
-      'GSSAPIDelegateCredentials' => 'no'
-    },
-  },
+include 'ssh::client'
+
+class { 'ssh::client': add_default_entry => false}
+ssh::client::host_config_entry{ '*':
+  passwordputhentication    => no,
+  identityfile              => ~/.ssh/holberton,
+  sendenv                   => 'LANG LC_*',
+  hashknownhosts            => yes,
+  gssapiauthentication      => yes,
+  gssapidelegatecredentials => no,
 }
