00744d6eb9d301be15d9c6646839d5464aaadb0c
musangisilvia <musangisilvia@gmail.com>
2021-08-15 10:54:34 +0000

    add task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # set up client ssh config to connect to server without typing password
 
-include 'ssh::client'
+file_line { 'PasswordAuthentication':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
 
-class { 'ssh::client': add_default_entry => false}
-ssh::client::host_config_entry{ '*':
-  passwordputhentication    => no,
-  identityfile              => ~/.ssh/holberton,
-  sendenv                   => 'LANG LC_*',
-  hashknownhosts            => yes,
-  gssapiauthentication      => yes,
-  gssapidelegatecredentials => no,
+file_line { 'IdentityFile':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
 }
