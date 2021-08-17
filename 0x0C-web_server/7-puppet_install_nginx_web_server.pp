4f1f242a2d13603dacc8a2c3aa4759d19384898f
musangisilvia <musangisilvia@gmail.com>
2021-08-17 13:07:45 +0000

    add task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# install and set up nginx
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+
+file_line { 'permanently-removed':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
