4c502364f311c19f838f90984dce408cdfb6e5a8
musangisilvia <musangisilvia@gmail.com>
2021-08-18 14:40:30 +0000

    add require and notify]

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,17 +5,20 @@ package { 'nginx':
 }
 
 file { '/var/www/html/index.html':
+  require => Package['nginx'],
   content => 'Holberton School',
 }
 
 file_line { 'add-rewrite':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'root /var/www/html;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+  ensure  => 'present',
+  require => Package['nginx'],
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'root /var/www/html;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+  notify  => Service['nginx'],
 }
 
 service { 'nginx':
   ensure  => running,
-  require => Package['nginx'],
+  require => File_line['add-rewrite'],
 }
