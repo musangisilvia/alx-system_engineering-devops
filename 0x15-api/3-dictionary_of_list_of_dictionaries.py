4e6e669137cecae84b0a2a45188d66aa2a2d0987
musangisilvia <musangisilvia@gmail.com>
2021-09-15 20:21:42 +0000

    add task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    A python script that, using a REST API,
+    returns information about all employeer TODO
+    list progress and exports it in CSV format in
+    json file.
+"""
+
+
+from sys import argv
+import requests
+import json
+
+
+if __name__ == "__main__":
+    emps = requests.get("https://jsonplaceholder.typicode.com/users").json()
+
+    employees = {}
+    for emp in emps:
+        empId = emp['id']
+        employees.update({empId: []})
+        task_url = "https://jsonplaceholder.typicode.com/users/{}/todos".\
+                   format(empId)
+        tasks = requests.get(task_url).json()
+
+        for task in tasks:
+            employees[empId].append({'username': emp['username'],
+                                     'task': task['title'],
+                                     'completed': task['completed']})
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(employees, f)
