7188036d3e197415c301772b2f88da8d3f4e53d2
musangisilvia <musangisilvia@gmail.com>
2021-09-15 20:04:49 +0000

    add task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    A python script that, using a REST API, for a given
+    employee ID, returns information about his/her TODO
+    list progress and exports it in CSV format in
+    python file.
+"""
+
+
+from sys import argv
+import requests
+import json
+
+
+if __name__ == "__main__":
+    empId = argv[1]
+    empname = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                           .format(empId)).json().get("name")
+
+    tasks = 0  # total number of tasks
+    c_tasks = []  # list of completed tas
+    r = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                     .format(empId)).json()
+
+    empJSON = {empId: []}
+
+    for tasks in r:
+        empJSON[empId].append({'task': tasks['title'],
+                               'completed': tasks['completed'],
+                               'username': empname})
+
+    with open('{}.json'.format(empId), 'w') as f:
+        json.dump(empJSON, f)
