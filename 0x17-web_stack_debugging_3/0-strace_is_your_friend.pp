b3ace788690d733f9c5ff66ef00d86779f579bab
musangisilvia <musangisilvia@gmail.com>
2021-10-01 14:04:55 +0300

    add task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# fix 500 - Internal server error
+exec { '/var/www/html/wp-settings.wp':
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
